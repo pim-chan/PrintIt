f5888bd10020ac470da7675c4129a0bc2236c11b
pim-chan <tamba.chloe@outlook.fr>
2023-06-10 02:13:00 +0200

    Version finale projet

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,72 @@
-// const slides = [
-// 	{
-// 		"image":"slide1.jpg",
-// 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-// 	},
-// 	{
-// 		"image":"slide2.jpg",
-// 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-// 	},
-// 	{
-// 		"image":"slide3.jpg",
-// 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-// 	},
-// 	{
-// 		"image":"slide4.png",
-// 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-// 	}
-// ]
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
 
-const slidesContainer = document.querySelector(".slides_container") 
-const slides = Array.from(slidesContainer.children);
-const nbSlides = slides.length;
-
-const nextBtn = document.querySelector(".arrow_right");
-const prevBtn = document.querySelector(".arrow_left");
-
-const dotsNav = document.querySelector(".dots");
-const dot = Array.from(dotsNav.children);
-
+const nbSlides = slides.length - 1;
 let currentSlide = 0;
 
-nextBtn.addEventListener("click", function () {
-        if(currentSlide < nbSlides - 1){
-        currentSlide++;
-    } else {
+const slidesContainer = document.querySelector(".slides_container");
+const nextBtn = document.querySelector(".arrow_right");
+const prevBtn = document.querySelector(".arrow_left");
+const dotsNav = document.querySelector(".dots");
+
+const dotCreation = () => {
+    slides.forEach((element,index) => {
+        const dotElement = document.createElement('div');
+        if(index === 0){dotElement.classList.add('dot_selected');}
+        dotElement.classList.add('dot');
+        dotsNav.appendChild(dotElement);
+    });
+}
+
+const dotselection = (dotIndex) => {
+    Array.from(document.querySelectorAll('.dot')).forEach((element,index) => {
+        element.classList.remove('dot_selected');
+        if(index === dotIndex){element.classList.add('dot_selected');}
+    });
+}
+
+const slide = (direction) => {
+    currentSlide = currentSlide + direction;
+    console.log('Position of slide', currentSlide, slides[currentSlide]);
+
+    if(currentSlide === -1){
+        currentSlide = nbSlides;
+    }else if(currentSlide > nbSlides){
         currentSlide = 0;
     }
-    slides.forEach((slide, index) => {
-        slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
-    });
+
+    document.querySelector('.slide_img').setAttribute('src', './assets/images/slideshow/'+slides[currentSlide].image);
+    document.querySelector('.tagLine').innerHTML = slides[currentSlide].tagLine;
+    dotselection(currentSlide);
+}
+
+nextBtn.addEventListener("click", () => {
+    console.log('Next Slide');
+    slide(1);
 });
 
-prevBtn.addEventListener("click", function () {
-    if(currentSlide > 0){
-        currentSlide--; 
-    } else {
-        currentSlide = nbSlides - 1;
-    }; 
-    slides.forEach((slide, index) => {
-        slide.style.transform = `translateX(${100 * (index - currentSlide)}%)`;
-    });
+prevBtn.addEventListener("click", () => {
+    console.log('Previous Slide');
+    slide(-1);
 });
+
+dotCreation();
 
 
 
